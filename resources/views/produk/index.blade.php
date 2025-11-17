701eaf492e0352dbbd4d69b7962762c3c400368e
Agung Dwi Laksana <agung.laksana16@gmail.com>
2025-11-17 14:20:29 +0700

    menampilkan data menggunakan datatables yajra
    menampilkan data menggunakan datatables

--- resources/views/produk/index.blade.php
+++ resources/views/produk/index.blade.php
@@ -7,13 +7,25 @@
 
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://cdn.datatables.net/2.3.5/css/dataTables.bootstrap4.min.css">
+    <link rel="stylesheet" href="https://cdn.datatables.net/1.13.5/css/dataTables.bootstrap4.min.css">
+
 
     <title>Hello, world!</title>
   </head>
   <body>
-    <div class="contaiiner">
-        <h1>Laravel + Yajra</h1>    
+    <div class="container">
+        <h1>Laravel + Yajra</h1>
+        <hr>
+        <table class="table table-bordered" id="produk-table">
+            <thead>
+              <tr>
+                <th>ID</th>
+                <th>Nama</th>
+                <th>Harga</th>
+                <th>Stok</th>
+              </tr>
+            </thead>
+        </table>
     </div>
     
     <!-- Optional JavaScript; choose one of the two! -->
@@ -23,8 +35,25 @@
     integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo="
     crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-Fy6S3B9q64WdZWQUiU+q4/2Lc9npb8tCaSX9FK7E8HnRr0Jz8D6OP9dO5Vg3Q9ct" crossorigin="anonymous"></script>
-    <script src="https://cdn.datatables.net/2.3.5/js/dataTables.min.a"></script>
-    <script src="https://cdn.datatables.net/2.3.5/js/dataTables.bootstrap4.min.js"></script>
+    <script src="https://cdn.datatables.net/1.13.5/js/jquery.dataTables.min.js"></script>
+    <script src="https://cdn.datatables.net/1.13.5/js/dataTables.bootstrap4.min.js"></script>
+
+
+    <script>
+    $(function() {
+      $('#produk-table').DataTable({
+        processing: true,
+        serverSide: true,
+        ajax: '{!! url("/produk") !!}',
+        columns: [
+          { data: 'id', name: 'id' },
+          { data: 'name', name: 'name' },
+          { data: 'price', name: 'price' },
+          { data: 'stock', name: 'stock' }
+        ]
+      });
+    });
+    </script>
 
   </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,8 @@
 <?php
 
+use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
+use Yajra\DataTables\Facades\DataTables;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
-Route::get('/', function () {
-    return view('produk.index');
-});
+Route::get('/', 'ProductController@index');
+Route::get('/produk', 'ProductController@dataProduk');
